67a09a3914f3b6393b9cce3e8badf0729f1a1ae3
KyleCoffie <kyle.coffie@live.com>
2024-04-10 09:06:56 -0500

    first commit

---
+++ 4-1-2024 HWA.py
@@ -0,0 +1,12 @@
+# Buggy code
+
+number = int(input("Enter a number:"))
+
+if number > 0:
+    print("The number is positive.")
+
+elif number < 0 :
+    print("The number is negative")
+
+elif number == 0:
+    print("The number is 0")
---
+++ Leap Year Explorer.py
@@ -0,0 +1,11 @@
+#Task 1: Leap Year Checker
+
+year = int(input("Enter a year"))
+if type(year /4) is float:
+    print ("Not a Leap Year")
+
+elif type(year /100) is int:
+     print ("Not a Leap Year")
+
+elif type( year / 4) is int:
+    print ("Is a Leap Year")
