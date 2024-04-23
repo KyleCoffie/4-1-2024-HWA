32fbed63f184cb033290fd9d1eeffb87c7809989
KyleCoffie <kyle.coffie@live.com>
2024-04-22 20:16:37 -0500

    updated Leap Year

--- Leap Year Explorer.py
+++ Leap Year Explorer.py
@@ -1,11 +1,8 @@
 #Task 1: Leap Year Checker
 
 year = int(input("Enter a year"))
-if type(year /4) is float:
-    print ("Not a Leap Year")
+if (year % 4==0 and year % 100 !=0) or(year % 400 ==0):
+    print ("Leap Year")
 
-elif type(year /100) is int:
-     print ("Not a Leap Year")
-
-elif type( year / 4) is int:
-    print ("Is a Leap Year")
+else: 
+    print("Not a Leap Year")
